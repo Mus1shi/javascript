b0d9634ab33099d793135143c074744b69964614
mus1shi <tomvla49@gmail.com>
2024-06-17 12:03:29 +0200

    evolution 1

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 const listeMot = ["Cachalot","Pétunia","Serviette"]
-const listePhrases = ["Pas de panique !", "Lavie, l'univers et le reste", "Merci pour le poisson"]
+const listePhrases = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson"]
+
 let score= 0
 
-let choix = prompt("Veuillez coisir la liste : mots ou phrases")
+
+let choix = prompt("Veuillez choisir la liste : mots ou phrases")
     while(choix !== "mots" && choix !== "phrases") {
-            choix = prompt("veuillez choisir la liste : mots ou phrases")
+       choix = prompt("veuillez choisir la liste : mots ou phrases")
     }
 
     if (choix === "mots") {
         for (let i = 0; i < listeMot.length; i++) {
-    let motUtilisateur = prompt("Entrez le mot : " + listeMot[i])
-    if (motUtilisateur === listeMot [i]) {
-    score++ 
-  } 
+        let motUtilisateur = prompt("Entrez le mot :" + listeMot[i])
+        if (motUtilisateur === listeMot [i]) {
+        score++ 
+        }
+        console.log("Votre score est de " + score + " sur " + listeMot.length) 
+    } 
+    
+    }  else {
+       for (let i = 0; i < listePhrases.length; i++) {
+       let motUtilisateur = prompt("Entrez le mot :" + listePhrases[i])
+       if (motUtilisateur === listePhrases[i]) {
+       score++
+         }
+    }
+    console.log("Votre score est de " + score + " sur " + listePhrases.length)
 }
-}   else {
-    for (let i = 0; i < listeMot.length; i++) {
-        let motUtilisateur = prompt("Entrez le mot : " + listePhrases[i])
-        if (motUtilisateur === listePhrases [i]) {
-        score++
-  }
-}
-}
- 
 
-console.log("Votre score est de" + score + "sur" + listeMot.length)
